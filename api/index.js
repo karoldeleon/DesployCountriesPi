c31494f25ada42c4544121d5338a4adf4eaf23ab
karoldeleon <kfortuner29@hotmil.com>
2023-06-06 17:54:32 -0400

    commit

---
+++ api/index.js
@@ -0,0 +1,35 @@
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const getApiData = require('./src/controllers/getApiData');
+require('dotenv').config()
+const {
+  PORT
+} = process.env;
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => { //// aqui cambiar a alter luego de que la aplicacioneste lista para que no se reinicien los datos en la bd
+  ////esta linea es la conexion con el server 
+  server.listen(PORT, async () => {
+    await getApiData();
+    console.log('DataBase Created');
+    console.log('Server listening at 3001', PORT); // eslint-disable-line no-console
+  });
+});
